842cb35b7932ead9a9c223cbdbfacb6c046765ce
MEECHAI Vincent <43035257+MEECHAIVincent@users.noreply.github.com>
2021-02-22 14:33:27 +0100

    first commit

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Challenge 48H</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom for CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'inc/header.php'; ?>
+
+    <div class="container">
+        <h1> Hello World!! </h1>
+
+        <img src="img\photos_ambiance\_AJG3926_ok.jpg">
+        
+    </div>
+
+
+<?php include 'inc/footer.php'; ?>
