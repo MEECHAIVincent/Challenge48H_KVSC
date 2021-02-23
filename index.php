28c89939a6f32d853da27d0df9f2e0bdf3f9155b
KameliaTag <76104670+KameliaTag@users.noreply.github.com>
2021-02-23 15:17:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -38,10 +38,25 @@
 </head>
 
 <body>
-
+<nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Passion froid</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 <div class="wrapper">
 
-<div class="container">
     
   <div class="col-lg-12">
     <div class="col-lg-12">
